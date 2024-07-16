8a55ef51d3d1a8e6a01af275ed1c9754d9b8860c
Abdallah <fafafavd@gmail.com>
2024-07-16 16:40:57 +0300

    Added readall api for flights

---
+++ flights/readall.php
@@ -0,0 +1,20 @@
+<?php
+require "../connection.php";
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    $flight_number = $_Get["flight_number"];
+// ~:
+    $stmt = $connection->prepare('select * from flights where flight_number=?;');
+    $stmt->bind_param('s', $flight_number);
+    $stmt->execute();
+    $result=$stmt->get_result();
+
+    if ($result->num_rows>0){
+        $flights=$result->fetch_assoc();
+        echo json_encode(["flights"=>$flights]);
+    } else {
+        echo json_encode(["message"=>"no records were found"]);
+    }
+} else {
+    echo json_encode(["error" => "Wrong request method"]);
+}
