5b8031f334faa0cf8c5c2763fb93700cd9db01f2
Abdallah <fafafavd@gmail.com>
2024-07-16 16:45:07 +0300

    Added readall api for users

---
+++ users/readall.php
@@ -0,0 +1,20 @@
+<?php
+require "../connection.php";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $username = $_POST["username"];
+
+    $stmt = $connection->prepare('select * from users where username=?;');
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $result=$stmt->get_result();
+
+    if ($result->num_rows>0){
+        $user=$result->fetch_assoc();
+        echo json_encode(["users"=>$user]);
+    } else {
+        echo json_encode(["message"=>"no records were found"]);
+    }
+} else {
+    echo json_encode(["error" => "Wrong request method"]);
+}
